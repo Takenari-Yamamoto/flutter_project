a03da264eb78fec6e10557041dd5c9553c3ad972
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-09-15 17:21:09 +0900

    遷移

--- lib/TestPage1.dart
+++ lib/TestPage1.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'TestPage2.dart';
 
 class TestPage1 extends StatelessWidget {
   @override
@@ -9,8 +8,8 @@ class TestPage1 extends StatelessWidget {
           title: const Text("Test1"),
         ),
         body: Center(
-            child: TextButton(
-                onPressed: () => {Navigator.of(context).pushNamed("/test2")},
-                child: const Text("進む", style: TextStyle(fontSize: 80)))));
+            child: Container(
+                color: Colors.redAccent,
+                child: const Text("Test1", style: TextStyle(fontSize: 80)))));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'TestPage1.dart';
 import 'TestPage2.dart';
 import 'TestPage3.dart';
@@ -13,16 +14,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.red,
-        ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
-        routes: {
-          "/test1": (BuildContext context) => TestPage1(),
-          "/test2": (BuildContext context) => TestPage2(),
-          "/test3": (BuildContext context) => TestPage3(),
-        });
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
   }
 }
 
@@ -36,8 +33,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late PageController _pageController;
+  int _selectedIndex = 0;
+
+  // ボトムメニューの遷移先ページ
+  final _pages = [
+    TestPage1(),
+    TestPage2(),
+    TestPage3(),
+  ];
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _selectedIndex);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: TestPage1());
+    return Scaffold(
+        body: PageView(
+            controller: _pageController,
+            onPageChanged: _onPageChanged,
+            children: _pages));
   }
 }
